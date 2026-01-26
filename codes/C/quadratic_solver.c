6ad2bd6c7ffc0458bf9140a9aae7f5b6446d1ffa
akshaynikham <akshaynikhamrj+github@gmail.com>
2026-01-26 15:40:13 +0530

    solved  few problems and migrated NM notes as markdown without prpoer formating

---
+++ codes/C/quadratic_solver.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+double determinant(double matrix[3][3]) {
+    return matrix[0][0] * (matrix[1][1] * matrix[2][2] - matrix[1][2] * matrix[2][1]) -
+           matrix[0][1] * (matrix[1][0] * matrix[2][2] - matrix[1][2] * matrix[2][0]) +
+           matrix[0][2] * (matrix[1][0] * matrix[2][1] - matrix[1][1] * matrix[2][0]);
+}
+
+int main() {
+   
+    printf("Quadratic Solver Skeleton Ready.\n");
+    return 0;
+}
